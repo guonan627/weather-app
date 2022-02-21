102c0d9502af1db1274d9504e6ccca5b405fb7f6
Guo Nan <guonan@GuodeAir.gateway>
2022-02-22 01:45:34 +1000

    change api url

--- src/apis/open-weather-api.js
+++ src/apis/open-weather-api.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
 axios.defaults.baseURL= 'https://api.openweathermap.org/data/2.5/';
+const appIdQueryParm = `appid=${process.env.REACT_APP_API_KEY}`;
 
 function getCurrentWeather(location) {
   return axios.get(
-    `weather?q=${location}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+    `weather?q=${location}&units=imperial&${appIdQueryParm}`
   );
 }
 
 function getForecast(lat, lon){
   return axios.get(
-    `onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`
+    `onecall?lat=${lat}&lon=${lon}&${appIdQueryParm}`
   );
 }
 
